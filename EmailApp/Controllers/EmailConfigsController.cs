78541379cdcaa188411eb9cb9824d6bef93dc85d
mehdi golzari <mgmehdiforme@gmail.com>
2023-04-01 23:00:49 +0430

    remove comment

--- EmailApp/Controllers/EmailConfigsController.cs
+++ EmailApp/Controllers/EmailConfigsController.cs
@@ -60,8 +60,6 @@ namespace EmailApp.Controllers
         }
 
         // POST: EmailConfigs/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,UserId,From,SmtpServer,SmtpPort,UseSSLForSmtp,ImapServer,ImapPort,UseSSLForImap,UserName,Password")] EmailConfig emailConfig)
@@ -92,8 +90,6 @@ namespace EmailApp.Controllers
         }
 
         // POST: EmailConfigs/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,From,SmtpServer,SmtpPort,UseSSLForSmtp,ImapServer,ImapPort,UseSSLForImap,UserName,Password")] EmailConfig emailConfig)
